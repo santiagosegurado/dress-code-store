42bfc58b487f1aa3ed30f125d61805b3ce65e20f
Santiago Segurado <santisegurado1@gmail.com>
2023-09-14 02:35:52 -0300

    Sitemap arreglado

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://dress-code.com',
+      url: 'https://dress-code.store',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
